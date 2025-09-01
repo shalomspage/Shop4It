23e5d15b5060f04ab041bd24d0ca5a064bf7fa40
El-Brukevwe Shalom <shalomzpage@gmail.com>
2025-09-01 20:58:19 +0100

    feat(ui): hero backdrop style

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
       "url('https://images.unsplash.com/photo-1542219550-37153d387c27?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
   }}
 >
-  <div className="max-w-4xl mx-auto space-y-8 px-6 sm:px-12 md:px-16 lg:px-24 bg-white/80 pb-10 pt-10 rounded-xl shadow-lg">
+  <div className="max-w-4xl mx-auto space-y-8 px-6 sm:px-12 md:px-16 lg:px-24 bg-white/50 backdrop-blur-lg pb-10 pt-10 rounded-xl shadow-lg">
 
     <h1 className="text-4xl font-extrabold text-gray-900 leading-snug md:text-5xl">
       Find the Perfect Product for Every Occasion
