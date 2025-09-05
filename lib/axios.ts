72d61fe41e091369eddfd871144fc2304766e14e
El-Brukevwe Shalom <shalomzpage@gmail.com>
2025-09-05 22:21:25 +0100

    fix(axios): remove quotes around NEXT_PUBLIC_HOST so baseURL resolves correctly

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "process.env.NEXT_PUBLIC_HOST",
+  baseURL: process.env.NEXT_PUBLIC_HOST,
   headers: { "Content-Type": "application/json" },
 });
 
